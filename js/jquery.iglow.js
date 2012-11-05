b22683b3e73f4efab207d05bc4879bab551f158f
Dmitry Kharchenko <unclenorton@gmail.com>
2012-11-05 23:46:32 +0800

    Added general comments, corrected the default values, and used variable for SVG namespace

--- js/jquery.iglow.js
+++ js/jquery.iglow.js
@@ -1,5 +1,5 @@
 /**
- * @author Dims (unclenorton@gmail.com)
+ * @author Dmitry Kharchenko (unclenorton@gmail.com)
  * @link github.com/unclenorton
  * @requires jQuery
  *
@@ -11,6 +11,23 @@
  * $(selector).iGlow(config);
  *
  * config is an object contents configuraton parameters:
+ *
+ * - lighten: The number of times the image is blended onto itself to increase intensity.
+ * - turbulence, displace, dilate, erode: Values for respective SVG filters. Optional. It is recommended to use the default values,
+ * as increasing parameters may greatly affect performance. Defaults are 1 (except turbulence, which is 0).
+ * - blur: Standard deviation for Gaussian blur.
+ * - useImage {boolean}: Whether to show the original lightened image under the blurred one. Mainly for debugging purposes.
+ * - IEFalloff {0-100}: The second parameter for IE's falloff transparency filter. Default is 55. Arguably makes the effect
+ * look better over dark backgrounds when set to lower values.
+ * - fadeInSpeed: The parameter for jQuery.fadeIn animation used in showing the blur effect. Default is 500.
+ *
+ *
+ * TODO:
+ * 1. Implement CSS filters for supporting browsers (especially the upcoming version of Firefox).
+ * 2. Find a neater way for displaying blur with IE filters.
+ * 3. ?????
+ * 4. PROFIT!
+ * 
  */
 
 (function($) {
@@ -23,9 +40,9 @@
 	$.iGlow.conf = {
 		lighten: 2,
 		turbulence: 0,
-		displace: 5,
+		displace: 1,
 		dilate: 1,
-		erode: 6,
+		erode: 1,
 		blur: 14,
 		useImage: false,
 
@@ -48,10 +65,10 @@
 			var ieBlur = conf.blur;
 
 			if(supportsSvgBlur()) {
-				aImages[i] = new SVGGlow(jImage, i, conf);
+				aImages[i] = new SVGGlow(jImage, conf);
 			} else {
 				conf.ieBlur = ieBlur;
-				aImages[i] = new MSGlow(jImage, i, conf);
+				aImages[i] = new MSGlow(jImage, conf);
 				blurOffset = 1;
 			}
 
@@ -101,22 +118,25 @@
 	 */
 	
 	function SVGGlow(jImage, conf) {
-		var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-		svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+		var svgNS = 'http://www.w3.org/2000/svg';
+		var svg = document.createElementNS(svgNS, 'svg');
+		svg.setAttribute('xmlns', svgNS);
 		svg.setAttribute('version', '1.1');
 
+		// Set the safety padding for the canvas
 		svg.setAttribute('width', jImage.width() + conf.blur * 8);
 		svg.setAttribute('height', jImage.height() + conf.blur * 8);
 
-		defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+		defs = document.createElementNS(svgNS, 'defs');
 
-		var filter = document.createElementNS('http://www.w3.org/2000/svg', 'filter');
+		var filter = document.createElementNS(svgNS, 'filter');
 		filter.setAttribute('id', 'gaussian_blur');
 
 		// Lighten
-		var fblend = document.createElementNS('http://www.w3.org/2000/svg', 'feBlend');
+		var fblend = document.createElementNS(svgNS, 'feBlend');
 		for(var il = 0; il < conf.lighten; il++) {
-			fblend = document.createElementNS('http://www.w3.org/2000/svg', 'feBlend');
+			fblend = document.createElementNS(svgNS, 'feBlend');
 			fblend.setAttribute('in', ((il) ? 'lightened' : 'SourceGraphic'));
 			fblend.setAttribute('result', 'lightened');
 			fblend.setAttribute('mode', 'screen');
@@ -124,7 +144,7 @@
 		}
 
 		if(conf.turbulence) {
-			var fturbulence = document.createElementNS('http://www.w3.org/2000/svg', 'feTurbulence');
+			var fturbulence = document.createElementNS(svgNS, 'feTurbulence');
 			fturbulence.setAttribute('baseFrequency', "2");
 			fturbulence.setAttribute('numOctaves', "8");
 			fturbulence.setAttribute('seed', '125');
@@ -132,7 +152,7 @@
 			fturbulence.setAttribute('result', 'turb');
 			filter.appendChild(fturbulence);
 
-			var fdisplacement = document.createElementNS('http://www.w3.org/2000/svg', 'feDisplacementMap');
+			var fdisplacement = document.createElementNS(svgNS, 'feDisplacementMap');
 			fdisplacement.setAttribute('scale', conf.displace);
 			fdisplacement.setAttribute('xChannelSelector', 'R');
 			fdisplacement.setAttribute('yChannelSelector', 'B');
@@ -143,7 +163,7 @@
 		}
 
 		if (conf.dilate) {
-			fmorph = document.createElementNS('http://www.w3.org/2000/svg', 'feMorphology');
+			fmorph = document.createElementNS(svgNS, 'feMorphology');
 			fmorph.setAttribute('in', 'lightened');
 			fmorph.setAttribute('operator', 'dilate');
 			fmorph.setAttribute('radius', conf.dilate);
@@ -152,7 +172,7 @@
 		}
 		
 		if (conf.erode) {
-			var fmorph = document.createElementNS('http://www.w3.org/2000/svg', 'feMorphology');
+			var fmorph = document.createElementNS(svgNS, 'feMorphology');
 			fmorph.setAttribute('in', 'lightened');
 			fmorph.setAttribute('operator', 'erode');
 			fmorph.setAttribute('radius', conf.erode);
@@ -161,14 +181,14 @@
 		}
 		
 		if (conf.blur) {
-			var gblur = document.createElementNS('http://www.w3.org/2000/svg', 'feGaussianBlur');
+			var gblur = document.createElementNS(svgNS, 'feGaussianBlur');
 			gblur.setAttribute('in', 'lightened');
 			gblur.setAttribute('stdDeviation', conf.blur);
 			filter.appendChild(gblur);
 		}
 		
 		if(conf.useImage) {
-			fblend = document.createElementNS('http://www.w3.org/2000/svg', 'feBlend');
+			fblend = document.createElementNS(svgNS, 'feBlend');
 			fblend.setAttribute('in', 'SourceGraphic');
 			fblend.setAttribute('mode', 'lihgten');
 			filter.appendChild(fblend);
@@ -177,7 +197,7 @@
 		defs.appendChild(filter);
 		svg.appendChild(defs);
 
-		var simage = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+		var simage = document.createElementNS(svgNS, 'image');
 
 		simage.setAttribute('x', conf.blur * 5);
 		simage.setAttribute('y', conf.blur * 5);
@@ -207,6 +227,9 @@
 			lighten += "progid:DXImageTransform.Microsoft.Wave(freq=0, lightStrength=100, strength=1, add=1) ";
 		}
 
+		// It is essential to have the Blur filter be the last one. Failing to do so may result in black/dark
+		// pixels near the border.
+		// The second filter (Alpha) uses style=2 parameter to implement opacity falloff.
 		jClone.css({
 			filter: lighten
 					+" progid:DXImageTransform.Microsoft.Alpha(opacity=100, finishopacity=" + conf.IEFalloff +", style=2)"
